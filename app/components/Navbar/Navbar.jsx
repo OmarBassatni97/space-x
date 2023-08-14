2c4748e93bd5cae811e189ba4a1a298f70cd07ad
Omar Bassatni <omarbassatni@gmail.com>
2023-08-14 13:50:56 +0300

    create next app

---
+++ app/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='h-[70px] w-full'>
+        <div>
+            <span></span>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
