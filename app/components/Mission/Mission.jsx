e0b28ded4c3568bf26b1cfc1ab5ba8d2e464ddcb
Omar Bassatni <omarbassatni@gmail.com>
2023-08-14 15:52:00 +0300

    adding type writer

--- app/components/Mission/Mission.jsx
+++ app/components/Mission/Mission.jsx
@@ -2,7 +2,13 @@
 import Image from 'next/image'
 import React from 'react'
 import { motion } from 'framer-motion'
+import { useTypewriter } from 'react-simple-typewriter'
 const Mission = () => {
+    const [text] = useTypewriter({
+        words: [' taking humans to space', 'making our world a better place'],
+        loop: false,
+        delaySpeed: 3000
+    })
     return (
         <div className='flex justify-center h-screen'>
             <div className='flex justify-between w-[1440px] gap-5 items-center'>
@@ -10,15 +16,15 @@ const Mission = () => {
                     initial=
                     {{
                         opacity: 0,
-                        x: -500,
+
                     }}
                     animate={{
                         opacity: 1,
                         x: 0
                     }}
-                    transition={{ duration: 2 }}
+                    transition={{ duration: 2, delay: 1.5 }}
                     className='text-white w-[50%] flex flex-col gap-3'>
-                    <h1 className='uppercase font-semibold text-4xl tracking-wide'> taking humans to space</h1>
+                    <h1 className='uppercase font-semibold text-4xl tracking-wide'> {text}</h1>
                     <p>
                         In 20220, SpaceX returned America's ability to fly NASA astronauts to and from the International Space Station on American vehicles for the first time since 2011. In addition to flying astronauts to space for NASA, SpaceX's Dragon spacecraft can also carry commercial astronauts to Earth orbit, the ISS or beyond.
                     </p>
@@ -27,13 +33,13 @@ const Mission = () => {
                     initial=
                     {{
                         opacity: 0,
-                        x: 500,
+
                     }}
                     animate={{
                         opacity: 1,
                         x: 0
                     }}
-                    transition={{ duration: 2 }}
+                    transition={{ duration: 2, delay: 1.5, }}
                 >
                     <Image src='/assets/images/astronaut.webp' height={800} width={800} />
                 </motion.div>
