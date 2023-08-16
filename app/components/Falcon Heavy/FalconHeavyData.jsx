d6957dfd3262fe978980214eb6a947c17d34738a
Omar Bassatni <omarbassatni@gmail.com>
2023-08-16 15:27:23 +0300

    added falcon heavy data

---
+++ app/components/Falcon Heavy/FalconHeavyData.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import React from 'react'
+
+const FalconHeavyData = () => {
+    return (
+        <div className=' flex flex-col items-center '>
+            <div className='flex flex-col w-[1440px] justify-center gap-6 pt-11 text-white'>
+                <div className='flex flex-col gap-6 items-center'>
+                    <span className='text-8xl font-semibold'>5</span>
+                    <span className='uppercase '>total launches</span>
+                </div>
+                <div className='flex flex-col gap-6 items-center'>
+                    <span className='text-8xl font-semibold'>11</span>
+                    <span className='uppercase '>total landings</span>
+                </div>
+                <div className='flex flex-col gap-6 items-center'>
+                    <span className='text-8xl font-semibold'>6</span>
+                    <span className='uppercase '>total reflights</span>
+                </div>
+            </div>
+            <div className='text-white flex flex-col w-[440px] text-center justify-center items-center pt-10'>
+                <Image src='/assets/images/Falcon-heavy-icon.png' width={100} height={100} alt='falcon-heavy' />
+                <p>Falcon Heavy is composed of three reusable Falcon 9 nine-engine cores whose 27 Merlin engines together generate more than 5 million pounds of thrust at liftoff, equal to approximately eighteen 747 aircraft. As one if the world&apos;s most powerful operational rockets, Falcon Heavy can lift nearly 64 metric tons (141,000 lvs) to orbit</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default FalconHeavyData
--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 import FalconHeavy from './components/Falcon Heavy/FalconHeavy'
+import FalconHeavyData from './components/Falcon Heavy/FalconHeavyData'
 import Mission from './components/Mission/Mission'
 import StarLinkMission from './components/Starlink Mission/StarLinkMission'
 
 export default function Home() {
-  
+
   return (
     <div className=' overflow-x-hidden'>
       <Mission />
       <StarLinkMission />
       <FalconHeavy />
+      <FalconHeavyData />
     </div>
   )
 }
